509b48c19f51870f5f9079157424ceddb118bcf9
Sarmad <sarmadsa2@gmail.com>
2018-05-21 18:54:01 +0200

    Improve javadoc

---
+++ src/RaceResultRegister.java
@@ -0,0 +1,10 @@
+/**
+ * Represents a register that holds all avalible 
+ * race results, and provides some functionality 
+ * such as add, remove, find by name etc..
+ * 
+ * @author Sarmad
+ */
+public class RaceResultRegister {
+    
+}
